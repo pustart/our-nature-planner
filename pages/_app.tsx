c9203dfdb827fe16d8468ce75ad1532aa52bf18c
pustart <polaris9955@gmail.com>
2023-09-26 22:16:13 +0300

    refactor: update project structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.scss'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import '../src/styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
