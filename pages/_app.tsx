27772064dc3fcee357027c04c7d5a73c0d1005f2
pustart <polaris9955@gmail.com>
2023-09-27 21:42:15 +0300

    feat: add custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
-import '../src/styles/globals.scss'
+import '../src/styles/index.scss'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Наша Природа — календарь событий</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp
