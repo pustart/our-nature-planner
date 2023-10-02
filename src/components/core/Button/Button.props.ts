ccbe1d2911c1c513e70eabb32f5308d619e2a2af
pustart <polaris9955@gmail.com>
2023-10-02 18:07:07 +0300

    refactor: move btn appearance types to enum

--- src/components/core/Button/Button.props.ts
+++ src/components/core/Button/Button.props.ts
@@ -1,7 +1,12 @@
 import { AriaButtonOptions } from '@react-aria/button';
 import { ReactNode } from 'react';
 
+export enum Appearance {
+  DEFAULT = 'default',
+  OUTLINED = 'outlined',
+}
+
 export interface ButtonProps extends AriaButtonOptions<'button'> {
   children: ReactNode;
-  appearance?: 'default' | 'outlined';
+  appearance?: Appearance;
 }
