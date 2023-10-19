28f6472f5a9f94ecd7980bde939386a5d46864c1
pustart <polaris9955@gmail.com>
2023-10-19 22:52:49 +0300

    refactor: update types for layout HOC

--- src/layouts/Default/Layout.props.ts
+++ src/layouts/Default/Layout.props.ts
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-export interface LayoutProps {
+export interface LayoutProps extends Record<string, unknown> {
   children: ReactNode;
   isAuthorized: boolean;
 }
--- src/layouts/Default/Layout.tsx
+++ src/layouts/Default/Layout.tsx
@@ -1,11 +1,14 @@
 import React, {
-  FunctionComponent,
+  FC,
 } from 'react';
 import { LayoutProps } from './Layout.props';
 import { Header } from '../../components/modules';
 import s from './Layout.module.scss';
 
-function Layout({ children, isAuthorized = false }: LayoutProps): JSX.Element {
+function Layout({
+  children,
+  isAuthorized = false,
+}: LayoutProps): JSX.Element {
   return (
     <>
       <Header isAuthorized={isAuthorized}/>
@@ -14,11 +17,11 @@ function Layout({ children, isAuthorized = false }: LayoutProps): JSX.Element {
   );
 }
 
-export const withDefaultLayout = <T extends Record<string, unknown>>(
-  Component: FunctionComponent<T>,
+export const withDefaultLayout = <T,>(
+  Component: FC<T>,
   isAuthorized: boolean
 ) =>
-  function withLayoutComponent(props: T): JSX.Element {
+  function WithLayoutComponent(props: T & LayoutProps): JSX.Element {
     return (
       <Layout isAuthorized={isAuthorized}>
         <Component {...props} />
